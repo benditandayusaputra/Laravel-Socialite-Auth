b9700cdad1b3500f4839072d69546adf46517e53
benditandayusaputra <benditandayusaputra@gmail.com>
2021-01-20 13:53:16 +0700

    :truck: Add App

---
+++ app/SocialAccount.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SocialAccount extends Model
+{
+    /**
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
